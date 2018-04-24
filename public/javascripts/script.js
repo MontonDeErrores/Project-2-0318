1de5ac60de477b9479b71962f76ec2d78f3eb277
Pedro Piury Blanquez <pedropiuryblanquez@MacBook-Air-de-Pedro.local>
2018-04-24 19:53:13 +0200

    Mejoras en el mapa

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,7 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
 
   window.onload = () => {
-    let latitude = parseFloat($('#lat').text());    
+    let partyName = $('#partyname').text();
+    let latitude = parseFloat($('#lat').text()); 
+    let image = 'http://res.cloudinary.com/ignlopezsanchez/image/upload/v1524588458/newMarker.png';
+    var icon = {
+      url: "http://res.cloudinary.com/ignlopezsanchez/image/upload/v1524588458/newMarker.png", // url
+      scaledSize: new google.maps.Size(50, 80), // scaled size
+  };
+
+  var contentString = `Nombre: <b>${partyName}</b>`
+
+
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+
     console.log(latitude)
     let longitude = parseFloat($('#long').text());
     console.log(longitude)
@@ -10,23 +24,37 @@ document.addEventListener('DOMContentLoaded', () => {
       lng: longitude
     };
   
-    
-  
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 13,
       center: partyLocation
     });
   
-    
-  
     const partyMaker = new google.maps.Marker({
+
       position: {
         lat: latitude,
         lng: longitude
       },
       map: map,
-      title: "I'm here"
+      animation: google.maps.Animation.DROP,
+      title: partyName,
+      icon: icon
     });
+    partyMaker.addListener('click', toggleBounce);
+
+    partyMaker.addListener('click', function() {
+      infowindow.open(map, partyMaker);
+      toggleBounce;
+    });
+
+
+    function toggleBounce() {
+      if (partyMaker.getAnimation() !== null) {
+        partyMaker.setAnimation(null);
+      } else {
+        partyMaker.setAnimation(google.maps.Animation.BOUNCE);
+      }
+    }
     // locationPlaces.forEach(places => {
     //   const pos = {
     //     lat: event.location.coordinates[1],
