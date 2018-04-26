a2d5c7b42046ff2b82d5af41f25487654b8ab778
Pedro Piury Blanquez <pedropiuryblanquez@MacBook-Air-de-Pedro.local>
2018-04-26 12:42:01 +0200

    Arreglada variable

--- mail/transporterGmail.js
+++ mail/transporterGmail.js
@@ -6,7 +6,7 @@ const transporter = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
       user: 'fiestit@gmail.com',
-      pass: process.env.$MAILPASS
+      pass: process.env.MAILPASS
     }
 });
 
